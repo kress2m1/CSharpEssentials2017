706b30433ed9eaf5997e03929f8eb95045ac0184
Yetty2010 <yetty2010@hotmail.com>
2017-08-16 12:07:53 +0100

    Added greetings to Titibasic solution

---
+++ TitiBasicSoln/TitiBasic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TitiBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Hello");         //This  is introduction to Csharp
+          Console.WriteLine("Bryan Divas");
+
+          Console.Write("Hello");  
+          Console.Write("Bryan");
+
+          Console.ReadKey();
+        }
+
+
+    }
+}
