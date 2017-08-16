2e35eb997a2b839e632dc418707c38e92b001527
Yetty2010 <yetty2010@hotmail.com>
2017-08-16 14:40:10 +0100

    updated with Mathematical Functions

--- TitiBasicSoln/TitiBasic/Program.cs
+++ TitiBasicSoln/TitiBasic/Program.cs
@@ -16,7 +16,67 @@ namespace TitiBasic
           Console.Write("Hello");  
           Console.Write("Bryan");
 
+
+          Console.WriteLine();
+          string name = "Titi";
+            
+          Console.WriteLine("My name is " + name);
+           
+          string welcome = "Hello Bryan Divas";
+          Console.WriteLine(welcome);
+
+          Console.WriteLine();
+          Console.WriteLine();
+
+
+            //This is Mathematical function
+            //Addition with concartination
+          int numOne = 20;
+          int numTwo = 30;
+          int result = numOne + numTwo;
+          Console.WriteLine("20 + 30 = " + result);
+
+          result = numOne * numTwo;
+          Console.WriteLine("This result in: " + result);
+          result = numTwo - numOne;
+          Console.WriteLine(result);
+            
+          Console.WriteLine();
+          double newResult = numTwo / numOne;
+          Console.WriteLine(newResult);
+
+          Console.WriteLine();
+          newResult = numTwo % numOne;
+          Console.WriteLine("30 % 20 = " + newResult);
+
+
+         // This is if statement
+          if (numOne < numTwo)
+          {
+          Console.WriteLine("numoTwo is > numOne");
+           }
+           else
+          {
+          Console.WriteLine("numOne is < numTwo");
+
+          }
+           
+
           Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
 
 
