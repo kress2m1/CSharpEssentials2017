9b0c60d0fd93e8779c3f5852170fb1d17f780d02
Moj7 <fola_racheal@yahoo.com>
2017-08-17 04:44:04 +0100

    new branch

---
+++ AutoProject1/AutoProject1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello");
+
+            Console.WriteLine("World");
+
+            Console.WriteLine();
+            string name = "Rachael";
+            Console.WriteLine("My name is" + name);
+
+
+
+
+            Console.WriteLine();
+            int numOne = 10;
+            int numTwo = 30;
+            int result = numOne + numTwo;
+            Console.WriteLine(result);
+
+            Console.WriteLine();
+            result = numOne + numTwo;
+            Console.WriteLine(result);
+
+            Console.WriteLine();
+            result = numOne*numTwo;
+            Console.WriteLine( result);
+
+            Console.WriteLine();
+            result = numTwo - numOne;
+            Console.WriteLine(result);
+
+            Console.WriteLine();
+            Double newResult = numTwo/numOne;
+            Console.WriteLine( newResult);
+
+            Console.WriteLine();
+            newResult = numTwo%numOne;
+            Console.WriteLine("30 % 10:" + newResult);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
