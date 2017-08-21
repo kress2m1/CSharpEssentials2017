0a1b64dc3d6410d0f2fb72ef277bc34d3ca8855f
Andre Dada <kress2m1@gmail.com>
2017-08-21 21:33:10 +0100

    intro to classes

---
+++ Basics/C_Coding_Basics/Animal/Dog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Coding_Basics
+{
+    class Dog
+    {
+        //variables of object dog
+        int eyes = 2;
+        int legs = 4;
+        int ears = 2;
+        int tail = 1;
+        int mouth = 1;
+
+        //methods of object Dog
+
+        //Our dog can eat
+        public void eat()
+        {
+            Console.WriteLine("I can eat");
+        }
+
+        public void bark()
+        {
+            Console.WriteLine("I can bark");
+        }
+
+        public void run()
+        {
+            Console.WriteLine("I can run");   
+        }
+
+        public void drink()
+        {
+            Console.WriteLine("I can drink");   
+        }
+    }
+}
--- Basics/C_Coding_Basics/Program.cs
+++ Basics/C_Coding_Basics/Program.cs
@@ -53,46 +53,127 @@ namespace C_Coding_Basics
             //    Console.WriteLine("numOne is < numTwo");
             //}
 
-            Console.WriteLine("Please enter a name:");
-            string userInput = Console.ReadLine();
+            //Console.WriteLine("Please enter a name:");
+            //string userInput = Console.ReadLine();
 
-            if (userInput.Equals("Kay"))
-            {
-                Console.WriteLine("Hello Kay");
-            }
-            else if (userInput.Equals("Titi"))
-            {
-                Console.WriteLine("Welcome Titi");
-            }
-            else if (userInput.Equals("Ayo"))
-            {
-                Console.WriteLine("Welcome Ayo");
-            }
-            else
-         
-            {
-                Console.WriteLine("Sound the alarm");
-            }
+            //if (userInput.Equals("Kay"))
+            //{
+            //    Console.WriteLine("Hello Kay");
+            //}
+            //else if (userInput.Equals("Titi"))
+            //{
+            //    Console.WriteLine("Welcome Titi");
+            //}
+            //else if (userInput.Equals("Ayo"))
+            //{
+            //    Console.WriteLine("Welcome Ayo");
+            //}
+            //else
 
-            //SWITCH STATEMENT
-            Console.WriteLine("Please enter a food item");
-            string foodItem = Console.ReadLine();
+            //{
+            //    Console.WriteLine("Sound the alarm");
+            //}
 
-            switch (foodItem)
-            {
-                case "Beans":
-                    Console.WriteLine("We have beans"); 
-                    break;
-                case "Bread":
-                    Console.WriteLine("We have bread");
-                    break;
-                default:
-                    Console.WriteLine("The food item entered is not sold here");
-                    break;  
-            }
+            ////SWITCH STATEMENT
+            //Console.WriteLine("Please enter a food item");
+            //string foodItem = Console.ReadLine();
+
+            //switch (foodItem)
+            //{
+            //    case "Beans":
+            //        Console.WriteLine("We have beans"); 
+            //        break;
+            //    case "Bread":
+            //        Console.WriteLine("We have bread");
+            //        break;
+            //    default:
+            //        Console.WriteLine("The food item entered is not sold here");
+            //        break;  
+            //}
+
+            //int enterNumber = int.Parse(Console.ReadLine());
+            //if (enterNumber > 0)
+            //{
+            //    Console.WriteLine("You have entered a number");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Wetin you enter?");
+            //}
+
+            //Console.WriteLine("Please enter a number: ");
+            //string number = Console.ReadLine();
+            //float numberConverted = float.Parse(number);
+
+            //Console.WriteLine(numberConverted / 100);
+            //Console.WriteLine(number + 100);
+
+            /**
+             * int == datatype
+             * numberToBeChecked == variable name
+             * 32 == the value assigned
+             */
+
+            //int numberToBeChecked = 32;
+
+            //while (numberToBeChecked < 40)
+            //{
+            //    Console.WriteLine("Hello");
+            //    numberToBeChecked = numberToBeChecked + 4;
+            //}
+
+            //Console.WriteLine("Hello, I am free!!");
+
+            //Console.WriteLine("Please enter the patient name: ");
+            //string patientName = Console.ReadLine();
+            //Console.WriteLine("Only give insulin injection to Agnes");
+
+            //do
+            //{
+            //    Console.WriteLine("Please give Insulin Injection");
+            //    break;
+            //} while (patientName.Equals("Agnes"));
+
+            //Console.WriteLine("Do not give injection if patient is Bob");
+
+            //FOR LOOP
+            //double counter;
+
+            //for (int counter = 0; counter <= 12; counter+=4)
+            //{
+            //    Console.WriteLine("My number is" + counter);
+            //}
+
+            //Console.WriteLine("Please enter your grades");
+            //int grades = int.Parse(Console.ReadLine());
+
+            //if ((grades >= 0) && (grades <= 9))
+            //{
+            //    Console.WriteLine("Failed Course");
+            //}
+            //else if ((grades >= 10) && (grades <= 20))
+            //{
+            //    Console.WriteLine("Passed");
+            //} else
+            //{
+            //    Console.WriteLine("Who you be?");
+            //}
+
+            // Class --> Object = New Instance --> Constructor
+            Dog husky = new Dog();
+            Console.WriteLine("This is a Husky");
+            husky.bark();
+
+            Console.WriteLine();
+            Console.WriteLine("This is a Hush Puppy");
+            Dog hushPuppy = new Dog();
+            hushPuppy.bark();
+            hushPuppy.drink();
+            hushPuppy.eat();
 
 
-
+            
+           
 
             Console.ReadKey();
         }
