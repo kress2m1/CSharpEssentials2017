0cc54aa7fb15428f59b36be228581c08fabd5213
Andre Dada <kress2m1@gmail.com>
2017-08-26 08:03:00 +0100

    Added new Projects for Access Modifiers

--- Basics/C_Coding_Basics/ProgramMain.cs
+++ Basics/C_Coding_Basics/ProgramMain.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using C_Coding_Basics.Humans;
+using ExtendedFamily.Cousins;
+using ExtendedFamily.GrandParents;
+using ExtendedFamily.Nieces;
 using Family;
 
 
@@ -15,19 +18,27 @@ namespace C_Coding_Basics
         {
             
             // Class --> Object = New Instance --> Constructor
-            Dog husky = new Dog();
-            Console.WriteLine("This is a Husky");
-            husky.Bark();
+            //Dog husky = new Dog();
+            //Console.WriteLine("This is a Husky");
+            //husky.Bark();
 
-            Console.WriteLine();
-            Console.WriteLine("This is a Hush Puppy");
-            Dog hushPuppy = new Dog();
-            hushPuppy.Bark();
-            hushPuppy.Drink();
-            hushPuppy.Eat();
+            //Console.WriteLine();
+            //Console.WriteLine("This is a Hush Puppy");
+            //Dog hushPuppy = new Dog();
+            //hushPuppy.Bark();
+            //hushPuppy.Drink();
+            //hushPuppy.Eat();
 
-            Human humansTeen = new Human();
-            humansTeen.PullDetailsFromTeenager();
+            //Human humansTeen = new Human();
+            //humansTeen.PullDetailsFromTeenager();
+
+            NiecesClass niece = new NiecesClass();
+            niece.DisplayNieceName();
+
+            CousinsClass cousinsClass = new CousinsClass();
+            cousinsClass.DisplayNieceName();
+            cousinsClass.CallNieceFriendsName();
+            cousinsClass.CallDetailsForNiece();
 
 
             Console.ReadKey();
