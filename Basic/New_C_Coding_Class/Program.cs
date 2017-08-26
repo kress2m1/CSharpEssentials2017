228ef3905bdca2abf71c14ed8df5b358a21d05fb
presideji <presideji@yahoo.com>
2017-08-26 13:00:35 +0100

    Added New Code

--- Basic/New_C_Coding_Class/Program.cs
+++ Basic/New_C_Coding_Class/Program.cs
@@ -13,13 +13,17 @@ namespace New_C_Coding_Class
             Console.WriteLine("Hello");
             Console.WriteLine("World");
 
+            //Console.ReadKey();
+
+
             Console.WriteLine(); //this creates an empty space
 
             Console.Write("Hello");
+            Console.Write(" "); //this creates a space between the word "Hello" and "World". so we have Hello World
             Console.Write("World");
 
 
-            Console.ReadKey();
+            Console.ReadKey(); //read any key that user press
         }
     }
 }
