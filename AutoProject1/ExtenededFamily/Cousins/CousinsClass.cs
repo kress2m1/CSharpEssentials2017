9571246a80a9aca8f0e2c5a5b03bedd57cc81472
Moj7 <fola_racheal@yahoo.com>
2017-08-30 17:34:20 +0100

    Overloaded

---
+++ AutoProject1/ExtenededFamily/Cousins/CousinsClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtenededFamily.Nieces;
+
+namespace ExtenededFamily.Cousins
+{
+   public class CousinsClass : NiecesClass
+    {
+        private string CousinName = "Tayo";
+        private string CousinLastName = "Gab";
+        public string CousinAddress = "City";
+        protected string CousinFriendsName = "Tokun";
+
+    }
+}
