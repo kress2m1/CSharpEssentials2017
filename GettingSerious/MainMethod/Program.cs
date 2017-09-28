9ee6e19e4fefd83dc0c40aa8ac5968f3e478821c
DESKTOP-L295UQ5\anita <anita.c.mak.28@gmail.com>
2017-09-28 23:34:09 +0200

    Object Oriented Prog

---
+++ GettingSerious/MainMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using MainMethod.ANIMALS;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1st create a CLASS  -> object = New Instance -> Constructor
+            
+            BIRDS Kpekpeye = new BIRDS();
+            Console.WriteLine("this a KPEKPEYE");
+            Kpekpeye.RUN();
+            Console.WriteLine();
+            Console.WriteLine("This is a TOLOTOLO");
+            BIRDS Tolotolo = new BIRDS();
+            Tolotolo.MakeAnnoyingNoises();
+            Tolotolo.SING();
+            Tolotolo.FLY();
+
+            Console.ReadKey();
+
+        }
+    }
+}
