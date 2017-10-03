beff87c5e4818ac1f44bd7c7b21b2545b70ea965
kbabatunde <ailaru29@gmail.com>
2017-10-03 23:23:52 +0100

    additional

---
+++ Grading System App1/ReconciliationMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using ReconciliationMethod.EndOfDayReconciliation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReconciliationMethod
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+
+        {
+           SalesReconciliation salesReconciliation = new SalesReconciliation();
+           salesReconciliation.TotalSales();
+           //string salesThree = TotalSales;
+           // Console.WriteLine("Total sales at end of Dayshift is:" + salesThree);
+
+            Console.ReadLine();
+        }
+    
+
+
+    }
+}
