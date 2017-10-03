64886bde9dd002e26e5ed5eedc78cee01a83717f
Andre Dada <kress2m1@gmail.com>
2017-10-03 07:47:01 +0100

    Added Solution for Selenium coding

---
+++ ShoppingCart/Com.ShoppingCart.Project/PageTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Com.ShoppingCart.Project
+{
+    [TestClass]
+    public class PageTest
+    {
+        private IWebDriver _driver;
+
+        [TestInitialize]
+        public void SetUpTest()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+        }
+
+        [TestMethod]
+        public void GetPageTitle()
+        {
+            Console.WriteLine("This is a test to get the page title");
+            var pageTitle = _driver.Title;
+            Assert.AreEqual(pageTitle, "My Store");
+        }
+
+        [TestMethod]
+        public void CallAnotherTest()
+        {
+            Console.WriteLine("This is another test");
+        }
+
+        [TestCleanup]
+        public void TearDownTest()
+        {
+            _driver.Quit();
+        }
+    }
+}
