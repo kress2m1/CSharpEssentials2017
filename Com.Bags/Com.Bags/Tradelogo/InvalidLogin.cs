67397f5539560637da1885df1e09a357e19201d9
Moj7 <fola_racheal@yahoo.com>
2017-10-25 21:53:56 +0100

    updated

--- Com.Bags/Com.Bags/Tradelogo/InvalidLogin.cs
+++ Com.Bags/Com.Bags/Tradelogo/InvalidLogin.cs
@@ -1,14 +1,71 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 
 namespace Com.Bags
 {
     [TestClass]
     public class InvalidLogin
     {
-        [TestMethod]
-        public void TestMethod1()
+        public object TestMethod { get; set; }
+
+        IWebDriver _driver;
+        private object usernameOrig;
+        private bool passwordOrig;
+    
+
+    
+        
+        [TestMethod, ("UAT")]
+        //[DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory |\\data.csv", "data#csv",
+        //  @"C:\Users\Rachel\Documents\CsharpRepo\CSharpEssentials2017\Com.Bags\Com.Bags\Tradelogo\InvalidLogin.csv,
+        //    "InvalidLogin.csv", DataAccessMethod.Sequential)]
+        public void InvalidUserLogin()
         {
+            var username = TestContext.DataRow["username"] as string;
+            var password = TestContext.DataRow["password"] as string;
+
+            Debug.Assert(usernameOrig != null, "usernameOrig ! = null");
+            var splitUserName = usernameOrig.Split("-");
+            Debug.Assert(passwordOrig ! = null, "passwordOrig ! = null");
+            var splitPassword = passwordOrig.Split("-");
+
+           username = splitUserName[1];
+            password = splitPassword[1];
+            //select login tab from the landing page
+            var loginTab = _driver.FindElement(By.ClassName("ico-login"));
+            loginTab.Click();
+            // enter email
+            if (username != null)
+            {
+                var loginEmail = _driver.FindElement(By.Id("Email"));
+                loginEmail.SendKeys(username);
+            }
+            // enter password
+            if (password != null)
+            {
+                var loginPassword = _driver.FindElement(By.Id("Password"));
+                loginPassword.SendKeys(password);
+            }
+            // click login button
+            var loginButton = _driver.FindElement(By.ClassName("login-button"));
+            loginButton.Click();
+            System.Threading.Thread.Sleep(4000);
+            var logoutButton = _driver.FindElement(By.ClassName("ico-logout"));
+            Assert.IsTrue(!logoutButton.Displayed);
         }
+
+        [TestCleanup]
+        public void ValidLogin()
+
+        }
+
+        private void DataSource(string microsoftVisualstudioTesttoolsDatasourceCsv, string datadirectoryDataCsv, string dataCsv, object C)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object TestCleanup { get; set; }
     }
 }
