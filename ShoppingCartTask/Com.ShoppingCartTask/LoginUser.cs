aa3a63b8777d979f0d87ca42d1fb0c73f3edaaff
presideji <presideji@yahoo.com>
2017-11-02 23:45:41 +0000

    Data Driven Testing using MSTest n CSV files for negative tesint1

--- ShoppingCartTask/Com.ShoppingCartTask/LoginUser.cs
+++ ShoppingCartTask/Com.ShoppingCartTask/LoginUser.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Threading;
+﻿using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace Com.ShoppingCartTask
 {
-
     [TestClass]
     public class LoginUsers
     {
@@ -29,18 +27,17 @@ namespace Com.ShoppingCartTask
             @"C:\Users\omo akin\Documents\Automation2017\CSharpEssentials2017\ShoppingCartTask\userLoginDetailsPresideji.csv",
             "userLoginDetailsPresideji.csv",
             DataAccessMethod.Sequential)] //this is how to use datasource
-
         public void LogUserIn()
         {
-            var username = TestContext.DataRow[0] as string;
-            var password = TestContext.DataRow[1] as string;
+            var username = TestContext.DataRow["username"] as string;
+            var password = TestContext.DataRow["password"] as string;
             //selectlogin tab from the landing page
             var loginTab = _driver.FindElement(By.ClassName("ico-login"));
             loginTab.Click();
 
             //enter email
             var loginEmail = _driver.FindElement(By.Id("Email"));
-         //   loginEmail.SendKeys("kala@yahoo.com"); // after the datasheet is atatched we do not need this any more
+            //   loginEmail.SendKeys("kala@yahoo.com"); // after the datasheet is atatched we do not need this any more
             loginEmail.SendKeys(username);
             //enter password
             var loginPassword = _driver.FindElement(By.Id("Password"));
@@ -53,21 +50,60 @@ namespace Com.ShoppingCartTask
 
             //logout button
             var logoutButton = _driver.FindElement(By.ClassName("ico-logout"));
-            Assert.IsTrue(logoutButton.Displayed);// check logout button is display
-            
-
-
-            
+            Assert.IsTrue(logoutButton.Displayed); // check logout button is display
         }
 
+        //------------------------------------------------------------------------
+        [TestMethod, TestCategory("Smoke")]
+        // public void GetPageTitle()
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            @"C:\Users\omo akin\Documents\Automation2017\CSharpEssentials2017\ShoppingCartTask\Com.ShoppingCartTask\DataFiles\invalidUserLoginDetailsPresideji.csv",
+            "invalidUserLoginDetailsPresideji.csv",
+            DataAccessMethod.Sequential)] //this is how to use datasource
+        public void InvalidUserLogin()
+        {
+            var usernameOrig = TestContext.DataRow["username"] as string;
+            var passwordOrig = TestContext.DataRow["password"] as string;
+
+            //Debug.Assert(usernameOrig != null, "usernameOrig != null");
+            var splitUsername = usernameOrig?.Split('-'); //make refrenece to the split in the note
+           // Debug.Assert(passwordOrig != null, "passwordOrig != null");
+            var splitPassword = passwordOrig?.Split('-');
+
+            var username = splitUsername?[1];
+            var password = splitPassword?[1];
+
+            //select login tab from landing page
+            var loginTab = _driver.FindElement(By.ClassName("ico-login"));
+            loginTab.Click();
+
+            //enter email
+            var loginEmail = _driver.FindElement(By.Id("Email"));
+            //loginEnail.Sendkeys("kala@gmail.com")
+            loginEmail.SendKeys(username);
+
+            //enter password
+            var loginPassword = _driver.FindElement(By.Id("Password"));
+            //loginPassword.Sendkeys("welcome123");
+            loginPassword.SendKeys(password);
+
+
+            //click login button
+            var loginButton = _driver.FindElement(By.ClassName("login-button"));
+            loginButton.Click();
+
+            //   var logoutButton = _driver.FindElement(By.ClassName("ico-logout"));
+            // if (logoutButton == null) throw new ArgumentNullException(nameof(logoutButton));
+
+            // Assert.IsTrue(!logoutButton.Displayed);
+        }
 
         [TestCleanup]
         public void TearDownTest()
         {
-            Thread.Sleep(10000); //it is a type of wait 
+            //Thread.Sleep(10000); //it is a type of wait 
             _driver.Quit(); // will close everything
             //_driver.Close(); //will only close the most recent one
-
         }
     }
 }
