e73905ad9eea712247eb9b18a864ce724f6837b2
Andre Dada <kress2m1@gmail.com>
2017-11-21 07:12:45 +0000

    Andre: eCommerceShop Automation Framework

---
+++ com.OnlineStore/eCommerceTest/OpenCartTest/OpenCartHomepageValidationTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using eCommerce.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace eCommerceTest.OpenCartTest
+{
+    [TestClass]
+    public class OpenCartHomepageValidationTest
+    {
+        [TestMethod]
+        public void CheckHomePageTitle()
+        {
+            Console.WriteLine(OpenCartFramework.Default.Browser);
+            Console.WriteLine("This is MSTest");
+        }
+    }
+}
