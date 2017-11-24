c152d723b4669679411fb10fdb701f27fa91c8ee
Andre Dada <kress2m1@gmail.com>
2017-11-24 20:51:42 +0000

    Updated MSTest Automation Framework Code

--- OnlineShop/eCommerceOnlineShop/BrowserFactory/BrowserRepo.cs
+++ OnlineShop/eCommerceOnlineShop/BrowserFactory/BrowserRepo.cs
@@ -1,25 +1,30 @@
 ﻿using System;
-using eCommerce.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eCommerceOnlineShop.Configuration;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 
-namespace eCommerce.BrowserFactory
+namespace eCommerceOnlineShop.BrowserFactory
 {
     public class BrowserRepo
     {
         private IWebDriver _driver;
+
         public BrowserRepo InitializeBrowser()
         {
-            var browser = OpenCartFramework.Default.Browser;
+            var browser = eCommerceConfig.Default.Browser;
 
-            if (browser.ToLower().StartsWith("chr") || 
-                browser.ToLower().EndsWith("ome") || 
+            if (browser.ToLower().StartsWith("chro") ||
+                browser.ToLower().EndsWith("ome") ||
                 browser.ToLower().Equals("chrome"))
             {
-                 _driver = new ChromeDriver();
+                _driver = new ChromeDriver();
             }
-            else if (browser.ToLower().StartsWith("fire") ||
+            else if (browser.ToLower().StartsWith("fire") || 
                 browser.ToLower().EndsWith("fox") ||
                 browser.ToLower().Equals("firefox"))
             {
@@ -28,9 +33,10 @@ namespace eCommerce.BrowserFactory
             return new BrowserRepo();
         }
 
-        public IWebDriver SetUpBrowser()
+        public IWebDriver SetupBrowser()
         {
             _driver.Manage().Window.Maximize();
+            //_driver.Navigate().GoToUrl("http://www.opencart.com");
             return _driver;
         }
 
---
+++ OnlineShop/eCommerceOnlineShop/Hooks/CartHooks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eCommerceOnlineShop.BrowserFactory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace eCommerceOnlineShop.Hooks
+{
+    [TestClass]
+    public class CartHooks
+    {
+        private IWebDriver _driver;
+        private BrowserRepo _browserRepo;
+
+        [TestInitialize]
+        public void StartTest()
+        {
+            _browserRepo = new BrowserRepo();
+            _driver = _browserRepo
+                .InitializeBrowser()
+                .SetupBrowser();
+        }
+
+        [TestCleanup]
+        public void ShutDownTest()
+        {
+            _browserRepo.CloseBrowser();   
+        }
+
+    }
+}
