e0cae406f25350ee385497fb268fd68c0c1b7c6a
kbabatunde <ailaru29@gmail.com>
2017-11-27 22:54:21 +0000

    new solution

---
+++ RetconShopping/ReconOnlineShopping/Test/SampleTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RetconShoppingProject.Hookes;
+
+namespace ReconOnlineShopping.Test
+{
+        [TestClass]
+        public class SampleTest : CartHookes
+        {
+
+            [TestInitialize]
+            public void InitializeBrowser()
+            {
+
+            }
+
+            [TestMethod]
+            public void CheckHomePageTitle()
+            {
+                Console.WriteLine("Print Homepage Title");
+            }
+
+
+        }
+    
+}
+
---
+++ RetconShopping/RetconShoppingProject/Hookes/CartHookes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using RetconShoppingProject.BrowserFactory;
+
+namespace RetconShoppingProject.Hookes
+{
+    [TestClass]
+    public class CartHookes
+ 
+    {
+        private IWebDriver _driver;
+        private Browser _browser;
+
+        [TestInitialize]
+        public void StartTest()
+        {
+            _browser = new Browser();
+            _driver = _browser
+                .InitializeBrowser()
+                .SetupBrowser();
+        }
+
+        [TestCleanup]
+        public void ShutDownTest()
+        {
+            _browser.CloseBrowser();
+        }
+
+    }
+}
+
